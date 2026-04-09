83b31df7bf211c2f31722219886497da3458e91b
Angel Flores <20253ds147@utez.edu.mx>
2026-04-08 22:30:08 -0600

    Implementacion de carpetas en module-info para el correcto uso de los controllers, modelos, repositorios, servicios y vistas

--- Integradora/src/main/java/module-info.java
+++ Integradora/src/main/java/module-info.java
@@ -4,5 +4,16 @@ module com.example.integradora {
 
 
     opens com.example.integradora to javafx.fxml;
+    opens com.example.integradora.controllers to javafx.fxml;
+    opens com.example.integradora.repositories to javafx.fxml;
+    opens com.example.integradora.model to javafx.fxml;
+    opens com.example.integradora.services to javafx.fxml;
+    opens com.example.integradora.views to javafx.fxml;
+
+
     exports com.example.integradora;
+    exports com.example.integradora.controllers;
+    exports com.example.integradora.model;
+    exports com.example.integradora.repositories;
+    exports com.example.integradora.services;
 }
